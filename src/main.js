940552e0d8190e037e4a7d8197aa0c26513a61c4
Tatiana Cifuentes Gaitán <taciga30@gmail.com>
2023-09-19 15:49:40 -0500

    renderItems y moviesTotal importadas al main.js

--- src/main.js
+++ src/main.js
@@ -1,55 +1,12 @@
-import {movieTotal} from "./dataFunctions.js";
+import { movieTotal } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
-
 import data from "./data/ghibli/ghibli.js";
 
-// en tu HTML donde deseas mostrar las películas.
 const moviesList = document.getElementById("root");
+window.addEventListener("load", () => {
+  moviesList.appendChild(renderItems(data.films));
+});
+console.log(renderItems);
 
-
-// Función para mostrar las películas en el DOM.
-function renderMovies() {
-  // Utiliza el método map() para crear un array de HTML para cada película.
-  const movieHTMLArray = data.films.map(
-    (film) => `
-    <ul>
-          <article class="pelicula">
-            <img
-              itemprop="image"
-              src="${film.poster}"
-              alt="Cartel"
-            />
-            <li
-              class="titulo_pelicula"
-              itemscope
-              itemtype="pelicula"
-              itemprop="title"
-            >${film.title}</li>
-          </article>
-        </ul>
-  `
-  );
-
-  // Convierte el array de HTML en una cadena y agrega al elemento <ul> en el DOM.
-  moviesList.innerHTML = movieHTMLArray.join("");
-  console.log(movieHTMLArray.join(""));
-}
-
-// Llama a la función para mostrar las películas cuando la página se carga.
-window.addEventListener("load", renderMovies);
-
-
-//ubicarlo en el html
-const contador = document.querySelector(".contador span")
-//contar la cantida de peliculas en el array
-function movieShow() { 
-  const totalImprime = movieTotal(data.films)
-  contador.innerText = totalImprime;
-};
-// cuando se carga la pagina,
-window.addEventListener("load",movieShow)
-
-
-
-
-//console.log(example, renderItems, data);*/
+window.addEventListener("load", () => movieTotal(data.films));
+console.log(movieTotal);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,24 @@
-// estas funciones son de ejemplo
+//const moviesList = document.getElementById("root");
+const ul = document.createElement("ul");
 
-export const renderItems = () => {
-  return 'example';
+export const renderItems = (data) => {
+  data.forEach(
+    (film) =>
+      (ul.innerHTML += `
+              <article class="pelicula">
+                <img
+                  itemprop="image"
+                  src="${film.poster}"
+                  alt="Cartel"
+                />
+                <li
+                  class="titulo_pelicula"
+                  itemscope
+                  itemtype="pelicula"
+                  itemprop="title"
+                >${film.title}</li>
+              </article>
+      `)
+  );
+  return ul;
 };
-
